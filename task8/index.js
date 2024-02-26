b5f9df1a146d9b174984d66d9a2d83812a6b427c
AyanMaity <subho7365@gmail.com>
2024-02-26 09:17:03 +0530

    new files added

---
+++ task8/index.js
@@ -0,0 +1,9 @@
+function saveToLocalStorage(event) {
+    event.preventDefault();
+    const username = event.target.username.value;
+    const email = event.target.email.value;
+    const phone = event.target.phone.value;
+    localStorage.setItem('Username', username);
+    localStorage.setItem('Email', email);
+  localStorage.setItem('Phone', phone); 
+        };
